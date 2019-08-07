b78534fda76af6fbb954c1cdc33f835778b02912
Michael Sundvick <msun284@aucklanduni.ac.nz>
2019-08-07 17:35:41 +1200

    Initial functional commit

---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+let g_terminal: vscode.Terminal;
+let terminal_open: boolean = false;
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Activating extension AMPL');
+
+	let disposable_1 = vscode.commands.registerCommand('ampl.openConsole', openAMPLConsole);
+	
+	let disposable_2 = vscode.commands.registerCommand('ampl.runFile', () => {
+		var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		let document = editor.document;
+		if (document.isUntitled) {
+			document.save();
+		}
+		else {
+			let ext = path.extname(document.fileName);
+			if (ext == '.dat') {
+				writeToConsole(`data "${document.fileName}";`);
+			} else if (ext == '.mod') {
+				writeToConsole(`model "${document.fileName}";`);
+			} else if (ext == '.run') {
+				writeToConsole(`include "${document.fileName}"`);
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable_1);
+	context.subscriptions.push(disposable_2);
+	
+	vscode.window.onDidCloseTerminal((terminal) => {
+		if (terminal === g_terminal) {
+			terminal_open = false;
+		}
+	});
+	
+	
+}
+
+export function openAMPLConsole() {
+	g_terminal = vscode.window.createTerminal({name: "AMPL"});
+	terminal_open = true;
+	writeToConsole('ampl');
+	g_terminal.show(true);
+}
+
+function writeToConsole(msg: string) {
+	if (!terminal_open) {
+		openAMPLConsole();
+	}
+	g_terminal.sendText(msg);
+}
+/*
+function activate(context) {
+   var currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
+   var currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
+   //...
+}
+
+*/
